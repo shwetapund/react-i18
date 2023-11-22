e3d2dda0b36f316e1787c1070916f1035760387c
shwetapund <pundshweta2020@gmail.com>
2023-11-22 14:05:37 +0530

    I18n (Internationalization) is ready

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import './Home.css'
+import I18n from './../../utils/I18n'
 
 function Home() {
+
+const usersCount = 20;
+
+
+
   return (
     <div>
-      <h1>QuickLinks</h1>
+
+      <h1>{I18n("welcomeMessage")}</h1>
+
+      <p>{I18n("normalMessage")}</p>
+
+      <h3>{I18n("greetingMessage")}</h3>
+
+      <h5>{I18n("endMessage")}</h5>
+
+      <select 
+      defaultValue={localStorage.getItem("lang")}
+      onChange={(e)=>{
+        localStorage.setItem("lang", e.target.value);
+        window.location.reload();
+      }}>
+        <option value='mr'>Marathi</option>
+        <option value='hi'>Hindi</option>
+        <option value='en'>English</option>
+      </select>
+
+      <p>
+        {I18n("userStatMessage", "<studentCount>",usersCount)}
+      </p>
     </div>
   )
 }
